fc81c91bfe12e9de35ff6afa2b632a6941d3aebb
KanSaz <kanz.kingz@gmail.com>
2022-11-09 17:48:13 +0700

    updateclient

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserDto } from './Dto_test/creat-user.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    this.appService.createUser(createUserDto);
+  }
+
+  @Get('analytics')
+  getAnalytics() {
+    return this.appService.getAnalytics();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateUserDto } from './Dto_test/creat-user.dto';
+import { CreateUserEvent } from './Event_test/creat-user.event';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('ANALYTICS') private readonly analyticsClient: ClientProxy,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+  
+  createUser(createUserRequest: CreateUserDto) {
+    const user_created = this.analyticsClient.emit(
+      'user_created',
+      new CreateUserEvent(createUserRequest.username,createUserRequest.password),
+    );
+    // const user_created =  this.analyticsClient.send({ cmd: 'user_created' }, new CreateUserEvent(createUserRequest.username,createUserRequest.password));
+    return user_created;
+  }
+
+  getAnalytics() {
+    const get_analytics = this.analyticsClient.send({ cmd: 'get_analytics' }, {})
+    return get_analytics;
   }
 }
